aa246af5b9a85ca00e12be2a6f99d44468fb6fd9
mehdi zaaboub <mehdi.zaaboub@MacBook-Air-de-mehdi.local>
2024-11-05 16:17:14 -0500

    Added webdoe webapp

---
+++ webdoe/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './hq720.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Déploiement réussi ! ;)
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
